553bb21b37af320d1d499ae0819fdd19ae42457d
marble-3 <64812361+marble-3@users.noreply.github.com>
2020-07-15 18:11:32 -0500

    Update inputoutput.py
    adding code

---
+++ inputoutput.py
@@ -0,0 +1,10 @@
+print ("welcome to my carb counting program")
+a = str(input("enter food: "))
+b = num = (input("quantity: "))
+c = {'apple': '10', 'banana': '20', 'orange': '30'}
+
+result = float(c[a]) * float(b)
+
+print("Carbohydrate total:")
+print(result)
+
